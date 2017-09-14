d38882a6bbc51f00551829b7aa2c23fa9a033d54
alex nenadov <alexn@uvic.ca>
2017-09-14 14:56:15 -0600

    VDHL Basic alu-type operations

---
+++ VHDL/add_unsigned.vhd
@@ -0,0 +1,33 @@
+-- How do I add two std_logic_vectors unsigned
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use ieee.numeric_std.all;
+entity alu is
+    Port ( in1 : in  std_logic_vector(15 downto 0);
+           in2 : in  std_logic_vector(15 downto 0);
+           clk : in  STD_LOGIC; -- Just in case, this design is async
+           result : out  std_logic_vector(15 downto 0));
+
+end alu;
+
+architecture Behavioral of alu is
+
+begin
+
+process(clk,in1,in2)
+	variable tmp_s_16 : SIGNED (15 downto 0);
+	variable tmp_l_16 : std_logic_vector(15 downto 0);
+	
+	tmp_s_16 := x"0000";
+	tmp_l_16 := x"0000";
+
+	-- Unsigned Subtract
+	tmp_l_16 := STD_LOGIC_VECTOR(unsigned(in1) + unsigned(in2));
+	tmp_s_16 := signed(tmp_l_16);
+
+	result <= std_logic_vector(tmp_s_16);
+
+	end process;			
+
+end Behavioral;
---
+++ VHDL/subtract_signed.vhd
@@ -0,0 +1,30 @@
+-- How do I subtract two std_logic_vectors signed
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use ieee.numeric_std.all;
+entity alu is
+    Port ( in1 : in  std_logic_vector(15 downto 0);
+           in2 : in  std_logic_vector(15 downto 0);
+           clk : in  STD_LOGIC; -- Just in case, this design is async
+           result : out  std_logic_vector(15 downto 0));
+
+end alu;
+
+architecture Behavioral of alu is
+
+begin
+
+process(clk,in1,in2)
+	variable tmp_s_16 : SIGNED (15 downto 0);
+	
+	tmp_s_16 := x"0000";
+
+	-- Signed Subtract
+	tmp_s_16 := signed(in1) - signed(in2);
+
+	result <= std_logic_vector(tmp_s_16);
+
+	end process;			
+
+end Behavioral;
